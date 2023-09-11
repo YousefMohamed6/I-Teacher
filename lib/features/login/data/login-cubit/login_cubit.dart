7275485d4132b7a33674d56b368c140016d0b763
Yousef Mohamed <yo7sef2002@gmail.com>
2023-09-12 00:06:52 +0300

    add user data from firebase

--- lib/features/login/data/login-cubit/login_cubit.dart
+++ lib/features/login/data/login-cubit/login_cubit.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:mrjoo/core/utils/constants/text.dart';
+import 'package:mrjoo/features/chat/data/model/user_model.dart';
 import 'package:mrjoo/features/login/data/login-cubit/login_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -23,6 +26,12 @@ class LoginViewCubit extends Cubit<LoginViewState> {
         email: email.text,
         password: password.text,
       );
+      var userBox = Hive.box<UserModel>(kUserBox);
+      var user = UserModel(
+        userId: FirebaseAuth.instance.currentUser!.uid,
+        userName: FirebaseAuth.instance.currentUser!.displayName!,
+      );
+      await userBox.add(user);
       emit(LoginSuccess());
     } on FirebaseAuthException catch (e) {
       emit(LoginFialure(errMessage: e.code));
