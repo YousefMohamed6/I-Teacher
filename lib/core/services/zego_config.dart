90a0976d6a6d335e7d0abd08d8af6ae5360a91eb
Youssef Mohamed <yo7sef2002@gmail.com>
2026-05-06 14:21:38 -0700

    chore: update ZegoConfig with valid appID and appSign credentials

--- lib/core/services/zego_config.dart
+++ lib/core/services/zego_config.dart
@@ -1,4 +1,5 @@
 class ZegoConfig {
-  static const int appID = 0; // TODO: Replace with your ZEGOCLOUD AppID
-  static const String appSign = ''; // TODO: Replace with your ZEGOCLOUD AppSign
+  static const int appID = 226182831;
+  static const String appSign =
+      '1dd90cba3b697b5b1acf5564ce2506b4a25d9afdf8da109d9472e0e7e25d119c';
 }
